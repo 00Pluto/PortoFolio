cd9cf5d1e0cd9c98b9a356c18ba0e3e776502523
Jayden Joseph <jayden.b.joseph0@gmail.com>
2025-10-09 15:46:02 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,1 @@
-// Porto Portfolio Interactive JavaScript
-// (Add any interactivity or solar system animations here)
-
-// Example: Animate planets (optional)
-document.addEventListener('DOMContentLoaded', function() {
-    // Simple rotation animation for planets (if you want to expand)
-    // You can add more effects!
-});
+// TO BE ADDED
